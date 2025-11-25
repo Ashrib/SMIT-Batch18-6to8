0df6fe575ae6a6ad591af195655e68b95948badb
Ashrib <asharib920@gmail.com>
2025-11-25 19:42:11 +0500

    javascript variable, alert

---
+++ JS/Intro/app.js
@@ -0,0 +1,28 @@
+// alert("welcome ali");
+// alert("thanks for coming ali");
+// window.alert("alert the text")
+
+
+// declare
+var greet = "Hello, Welcome to my website";
+var thanks = "thanks for coming ali";
+// alert(greet);
+// alert(thanks);
+
+
+
+// declare
+var a;
+// value assign
+a = "hello";
+
+
+var b = "some text";
+alert(b);
+
+b = "text changed";
+alert(b);
+
+
+b = "abcd";
+
