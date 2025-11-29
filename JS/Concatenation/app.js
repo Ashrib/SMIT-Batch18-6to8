da507ee47abf18e2cb99d9367c13dc457546f4a4
Ashrib <asharib920@gmail.com>
2025-11-29 19:45:25 +0500

    maths expressions and string contatenation

---
+++ JS/Concatenation/app.js
@@ -0,0 +1,11 @@
+var firstName = "ali";
+var lastName = "khan";
+var fullName = firstName + " " + lastName;
+
+console.log("Thankyou " + fullName);
+console.log("Hi, " + firstName);
+
+
+console.log(12+6+"2"+2/10);
+console.log("6"+"4");
+
---
+++ JS/maths/app.js
@@ -0,0 +1,67 @@
+var count = 50;
+console.log(count)
+
+count = count + 100;
+console.log(count)
+
+
+count = count - 70;
+console.log(count);
+
+
+var num = 0;
+num++;
+console.log(num);
+num++;
+console.log(num);
+num++; // num = num + 1
+num++; // num = num + 1
+num++; // num = num + 1
+num++; // num = num + 1
+num++; // num = num + 1
+console.log(num);
+
+var num2 = 10;
+
+num2--; // num2 = num2 - 1;
+num2--; // num2 = num2 - 1;
+num2--; // num2 = num2 - 1;
+num2--; // num2 = num2 - 1;
+
+console.log(num2);
+num2++;
+num2++;
+num2++;
+num2++;
+num2++;
+num2++;
+console.log(num2);
+
+
+var num3 = 5;
+var newNum = num3++;  //post increment
+console.log(num3);
+console.log(newNum);
+
+
+var num4 = 0;
+var newNum2 = ++num4; // pre-increment
+console.log(num4);
+console.log(newNum2);
+
+
+
+var num5 = 50;
+++num5;
+num5++;
+console.log(num5);
+
+
+num5--; // post decrement
+--num5; // pre decrement
+
+
+
+
+
+
