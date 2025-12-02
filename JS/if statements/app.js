e717a0d6bf78353250aa4949b97210c7c78c2ee8
Ashrib <asharib920@gmail.com>
2025-12-02 19:45:00 +0500

    prompt, if statments

---
+++ JS/if statements/app.js
@@ -0,0 +1,35 @@
+
+// var result = prompt("pass or fail");
+
+// if(result == 'pass') { 
+//     console.log("ok");
+// }
+
+// console.log("outside of if");
+
+
+// course enrollment of 25 age
+// var ageInput = prompt("enter your age");
+// console.log(ageInput);
+
+
+if(ageInput == 29){ 
+    console.log("course is available");
+}
+
+// == has no concern with data type
+// === has concern with data type
+
+if('10' <= 10){
+    console.log("yes");
+}
+
+if(10 !== 10){
+   console.log("it is not equal"); 
+}
+
+
+
+
+
+
---
+++ JS/prompts/app.js
@@ -0,0 +1,17 @@
+// var fnameInput = prompt("enter your first name", "abc");
+// var lnameInput = prompt("enter your last name", "xyz");
+
+// console.log(fnameInput);
+// console.log(lnameInput);
+
+// alert("welcome "+fnameInput + " " + lnameInput);
+// console.log("welcome "+fnameInput + " " + lnameInput);
+
+
+//empty string
+""
+
+var num = prompt("enter a number");
+console.log(num);
+
+
