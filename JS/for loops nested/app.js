a0ee589f5fe259d5a35778f51530ddf7abb6abc1
Ashrib <asharib920@gmail.com>
2025-12-18 19:35:51 +0500

    for loop nested

---
+++ JS/for loops nested/app.js
@@ -0,0 +1,40 @@
+
+
+// var firstNames = ['ali','usman','umar', 'adil','noman']; 
+// var lastNames = ['khan','ali','raza', 'rao','khan']; 
+
+
+var numbers = [34, 5, 8, 26, 78, 23]; // i
+var toMultipleNums = [50, 20, 40];
+
+// for(var i = 0; i<numbers.length; i++){ // outer loop ---5
+
+//     var result = 0;
+//     for(var j =0; j < toMultipleNums.length; j++){ //inner loop ---
+
+//         console.log("outer loop holds: "+ numbers[i] + ", inner loop holds: "+ toMultipleNums[j]) // 34 * 50
+//         result = numbers[i] * toMultipleNums[j];
+//         console.log("result: "+result)
+//     }
+// }
+
+
+
+
+// for(var i=1; i<6; i++){  // n=10
+//     console.log('hello') // n
+//     for(var j=1; j<4; j++){ // n=5
+//         console.log('inner loop'); // 10 * 5 === n^2
+//     }
+// }
+
+for (var i = 1; i < 6; i++) { // 5
+    console.log() //5
+    
+    for (var j = 1; j < 9; j++) { // 8
+        console.log(i) //5 * 8
+    }
+
+    console.log() //5
+}
+
--- JS/for loops/app.js
+++ JS/for loops/app.js
@@ -4,7 +4,7 @@
 //     console.log(i * 3); // 5
 // }
 var isPresent = false;
-var stdNames = ['ali','usman','umar', 'adil','noman']; //4 , 3
+var stdNames = ['ali','usman','umar', 'adil','noman', 'ali']; //4 , 3
 
 for(var i = 0; i < stdNames.length; i++){
     // console.log(stdNames[i] + " at index: "+ i);
@@ -14,11 +14,11 @@ for(var i = 0; i < stdNames.length; i++){
     }
 }
 
-if(isPresent == true){
-    console.log("yes talha is present");
-}else{
-    console.log(" talha is not present");
-}
+// if(isPresent == true){
+//     console.log("yes talha is present");
+// }else{
+//     console.log(" talha is not present");
+// }
 
 
 
@@ -37,10 +37,42 @@ var numbersData = [];
 
 
 
-var num = '3';
+// var num = '3';
 
-if(num % 2 == 0){
-console.log("even")
+// if(num % 2 == 0){
+// console.log("even")
+// }
+
+
+
+var cities = ['karachi', 'lahore','hyderabad'];
+
+
+var userInput = prompt("enter a city to search");
+var isCheck = false;
+
+for(var i=0; i<cities.length; i++){
+    if(userInput == cities[i]){
+        isCheck = true;
+        break;
+    }
 }
 
+if(isCheck == true){
+    console.log("city found")
+}
+else{
+    console.log("city not found")
+}
+
+
+var count = 0;
+var nameToCheck = 'ali';
+for(var i=0; i< stdNames.length; i++){
+    if(stdNames[i] == nameToCheck){
+        count++;
+    }
+}
+
+console.log("name count: "+ count)
 
