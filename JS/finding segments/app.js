4d953859c9e05f92b992e6eac92e6297a3deb9e7
Ashrib <asahrib920@gmail.com>
2026-01-01 19:37:22 +0500

    replacing string and rounding number

--- JS/finding segments/app.js
+++ JS/finding segments/app.js
@@ -8,16 +8,30 @@
 //     }
 // }
 
+var para = "the New Yorker magazine doesn't allow the phrase World War II. sometext World War II"
+
+// for(var i=0; i<para.length; i++){
+//     if(para.slice(i, i+12) == "World War II"){
+//         // console.log("found at index: ",i);
+//         para = para.slice(0, i) + "the Second World War" + para.slice(i+12)
+//     }
+// }
+// console.log(para);
+
+// var findIndex = para.indexOf('World War II');
+
+// if(findIndex != -1 ){
+//     para = para.slice(0, findIndex)  + "the Second World War" + para.slice(findIndex+12)
+// }
+
+// console.log(para);
+// console.log(para.lastIndexOf("the"));
+
+// mutable ---- immutable
 
 
-
-var para = "The New Yorker magazine doesn't allow the phrase World War II. sometext World War II"
-
-
-for(var i=0; i<para.length; i++){
-    if(para.slice(i, i+12) == "World War II"){
-        // console.log("found at index: ",i);
-        para = para.slice(0, i) + "the Second World War" + para.slice(i+12)
-    }
-}
+/// global replace
+para = para.replace(/World War II/g ,"the Second World War" );
 console.log(para);
+
+console.log(para.charAt(2));
---
+++ JS/rounding numbers/app.js
@@ -0,0 +1,10 @@
+
+var num = 2.001;
+console.log("round: ", Math.round(num) );
+
+var num2 = 2.99999;
+console.log("floor: ", Math.floor(num2) );
+
+var num3 = 4.000001;
+console.log("ceil: ", Math.ceil(num3) );
+
