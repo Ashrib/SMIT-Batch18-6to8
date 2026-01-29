2cc3cec5731cb6b3b865243a7dc38f6d3e5e0f38
Arham <arhamali63@gmail.com>
2026-01-29 20:18:47 +0500

    switch and while,do-while loop

---
+++ JS/switch statements/app.js
@@ -0,0 +1,15 @@
+
+var number = 10;
+
+switch(number){  //===
+    case 20 :
+        console.log('it is 20');
+        break;
+    case 50 : 
+        console.log('it is 50');
+        break;
+        
+    default:
+        console.log('default');
+
+}
---
+++ JS/while and do-while loop/app.js
@@ -0,0 +1,35 @@
+
+
+
+// var i = 0;
+// while(i<=4){
+//     console.log(i);
+
+//     i++;
+// }
+
+
+var userInput;
+// 
+function getUserInput(){ ///
+    userInput = +prompt('enter your age');
+    console.log(userInput)
+}
+// getUserInput()
+
+// while(userInput == 0){
+//     getUserInput();
+// }
+// console.log(userInput)
+
+
+
+// var i = 1;
+// do{
+    // console.log(i)
+    // getUserInput();
+    // i++;
+// } while(userInput == 0);
+
+
+
