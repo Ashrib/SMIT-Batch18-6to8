537bea4e1225794f78a739af22412a6f47584c39
Arham <arhamali63@gmail.com>
2026-02-14 20:04:18 +0500

    todo list complete

--- JS/todo list/app.js
+++ JS/todo list/app.js
@@ -1,5 +1,65 @@
 var todos = [];
-var todoParentElm = document.getElementById('todo-parent')
+var todoParentElm = document.getElementById('todo-parent');
+var todoToBeUpdate = null;
+var indexToBeUpdate = null;
+var updateBtn = document.getElementById('update_btn');
+var addBtn = document.getElementById('add_btn');
+
+function deleteTodo(id){
+    console.log(id);
+
+    for(var i=0; i<todos.length; i++){
+        // console.log(todos[i])
+
+        if( todos[i].id == id){
+            // console.log('found at index: '+ i);
+            todos.splice(i,1);  // this updates the array (remove the specific object of todo)
+            renderTodos(); // re-render the updated array
+        }
+    }
+
+}
+
+function editTodo(id){
+    console.log(id);
+    var todoInput = document.getElementById('todo-input');
+    
+    addBtn.style.display = 'none'
+    updateBtn.style.display = 'inline'
+    
+    for(var i=0; i<todos.length; i++){
+        if(id == todos[i].id){
+            console.log(todos[i]);
+            todoInput.value = todos[i].text;
+            todoToBeUpdate = todos[i];
+            indexToBeUpdate = i;
+        }
+        
+    }
+    
+}
+
+function updateTodo(){
+    var todoInput = document.getElementById('todo-input');
+    console.log(todoInput.value)
+
+
+    todos[indexToBeUpdate].text = todoInput.value;
+
+    // OR
+     
+    // todos.splice(indexToBeUpdate, 1, {
+    //     id: todoToBeUpdate.id,  // id from previous data
+    //     createdAt: todoToBeUpdate.createdAt, /// createAt from previous data
+    //     text: todoInput.value,/// new text value from input
+    // });
+    addBtn.style.display = 'inline';
+    updateBtn.style.display = 'none';
+    todoInput.value = '';
+
+    renderTodos();// re-render the updated array
+
+}
 
 function addTodo() {
     var todoInput = document.getElementById('todo-input');
@@ -9,6 +69,7 @@ function addTodo() {
     };
 
     var todoObj = {
+        id: (new Date().getTime()) + Math.floor(Math.random() * 999), /// generate random id
         text: todoInput.value,
         createdAt: new Date(),
     }
@@ -26,17 +87,19 @@ function addTodo() {
 // D - delete
 
 function renderTodos() {
-    todoParentElm.innerHTML = ''
+    todoParentElm.innerHTML = '';
     for (var i = 0; i < todos.length; i++) {
         // var pElm = document.createElement('p');
         // pElm.innerText =  todos[i];
         // todoParentElm.appendChild(pElm);
 
+        // OR
+
         todoParentElm.innerHTML += `<div class='todo'>
         <span>${todos[i].text}</span>
-        <button>delete</button>
+        <button onClick='editTodo(${todos[i].id})'>edit</button>
+        <button onClick='deleteTodo(${todos[i].id})'>delete</button>
         <span>${todos[i].createdAt.getDate()} - ${todos[i].createdAt.getMonth() + 1} - ${todos[i].createdAt.getFullYear()}</span>
         </div>`
     }
 }
-
