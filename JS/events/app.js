e4368824cd5e47740ae144d276b9fee777819dad
Arham <arhamali63@gmail.com>
2026-02-17 20:05:56 +0500

    events

---
+++ JS/events/app.js
@@ -0,0 +1,40 @@
+function inputFocus(e){
+    // console.log(e.target);
+    var targetElement = e.target;  //// getting input element by using 'event'
+    targetElement.style.padding = '15px';
+};
+
+function inputBlur(e){
+    // console.log(e.target);
+    var targetElement = e.target;  //// getting input element by using 'event'
+    targetElement.style.padding = '0px';
+
+    var emailError =  document.getElementById('email-error');
+    if(targetElement.value.indexOf('@') == -1 ){
+        emailError.style.display = 'inline-block';
+        return;
+    }
+    if(targetElement.value.indexOf('.') == -1 ){
+        emailError.style.display = 'inline-block';
+        return;
+    }
+     if(targetElement.value.indexOf(' ') !== -1 ){
+        emailError.style.display = 'inline-block';
+        return;
+    }
+    if(targetElement.value[targetElement.value.indexOf('.')+2] == undefined){
+         emailError.style.display = 'inline-block';
+        return;
+    }
+    emailError.style.display = 'none';
+};
+
+var images = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEbxGgmUGIvztvt2ySh0kFon3jc7OVe-QZ5Q&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2OZFoooeonih8ocEaM_0fU9jJ8lWvz-M0tg&s']
+
+function onBoxOver(e){
+    e.target.style.backgroundImage = `url('${images[0]}')`
+}
+
+function onBoxOut(e){
+    e.target.style.backgroundImage = `url('${images[1]}')`
+}
