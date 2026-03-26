0c510a1dc8ad8f58bca6ab3f6a5e3883a4b426f6
manaheeljamil <manaheelsmit@gmail.com>
2026-03-26 20:04:12 +0500

    local storage

--- JS/local storage/app.js
+++ JS/local storage/app.js
@@ -1,15 +1,36 @@
 // // local storage
 
-var userName = 'user123';
-window.localStorage.setItem("user1", userName); /// set data
+// var userName = 'user123';
+// window.localStorage.setItem("user1", userName); /// set data
 
-var data = window.localStorage.getItem("user1"); /// get data
-console.log("data => ", data);
+// var data = window.localStorage.getItem("user1"); /// get data
+// console.log("data => ", data);
 
-var users = [{name:'abc'},{name: 'xyz'}];
+// var users = [{name:'abc'},{name: 'xyz'}];
 
-var stringData = JSON.stringify(users);  /// convert array into string
-console.log(stringData);
+// var stringData = JSON.stringify(users);  /// convert array into string
+// console.log(stringData);
 
-window.localStorage.setItem('usersData', stringData);
+// window.localStorage.setItem('usersData', stringData);
+
+
+
+
+/// get data from local storage
+var localStorageData = window.localStorage.getItem('userName');
+console.log(localStorageData)
+
+if(localStorageData == null){
+    var userInput = prompt('enter name');
+    console.log(userInput);
+    if(userInput.length > 0 ){
+        window.localStorage.setItem('userName', userInput)
+    }
+}else{
+    console.log('userName => ' + localStorageData)
+}
+
+
+
+
 
