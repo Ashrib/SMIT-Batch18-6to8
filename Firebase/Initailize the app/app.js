08148625b17b62334da15c0258b6db6dbc1e352b
Arham <arhamali63@gmail.com>
2026-05-03 19:43:00 +0500

    read, create and delete doc from firestore

--- Firebase/Initailize the app/app.js
+++ Firebase/Initailize the app/app.js
@@ -1,12 +1,40 @@
-import { doc, db, addDoc, collection, getDocs } from './firebaseConfig.js'
+import { doc, db, addDoc, collection, getDocs,deleteDoc } from './firebaseConfig.js'
 
 let users = [];
 let addBtn = document.querySelector('#add-btn');
 let input = document.querySelector('#user-name');
+let usersDiv = document.querySelector('.users-container');
+
+let deleteUser = async (userId) => {
+    try {
+        await deleteDoc(doc(db, "users", userId));
+        console.log('user deleted!')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+let renderUsers = () => {
+    users.map((user) => {
+
+        let userCard = document.createElement('div');
+        userCard.id = user.uid
+        userCard.className = 'user-card'
+        userCard.innerHTML = `
+        <h2>${user.username}</h2>
+        <button class='userDelete-btn'
+        >delete</button>
+       `;
+        userCard.querySelector('.userDelete-btn')
+            .addEventListener('click', () => deleteUser(user.uid))
+
+        usersDiv.appendChild(userCard)
+    })
+}
 
 
 
-addBtn.addEventListener('click', async () => {
+addBtn.addEventListener('click', async () => {  /// Create
     try {
         const docRef = await addDoc(collection(db, "users"), {
             username: input.value,
@@ -20,29 +48,30 @@ addBtn.addEventListener('click', async () => {
 
 
 // fetch users data
-let fetchUsers = async () => {
+let fetchUsers = async () => { /// Read data
     try {
         const querySnapshot = await getDocs(collection(db, "users"));
         querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
+            // console.log(doc.id, " => ", doc.data());
             users.push({
-                id: doc.id,
-                ...doc.data()
-            })
-           
+                uid: doc.id,
+                ...doc.data(),
+            });
         });
 
-    } catch (error) { 
+    } catch (error) {
         console.error(error);
     }
-    
+
 }
 
-        
-fetchUsers().then(()=>{
 
-    console.log(users)
+fetchUsers().then(() => {
+
+    console.log( 'users => ',users)
+    renderUsers()
 })
+
 
 
 /// method : POST  --> body {}
