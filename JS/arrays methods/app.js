106ea1016da0502d73c30af73c822b23326adfa3
manaheel jamil <manaheelsmit@gmail.com>
2026-05-30 19:48:07 +0500

    reduce, flat, array methods

--- JS/arrays methods/app.js
+++ JS/arrays methods/app.js
@@ -39,10 +39,55 @@ var lengthOfArray = mixArray.length - 1;  // last index of array
 
                                 // -1
 console.log( mixArray.slice(0, lengthOfArray) );
-
 console.log( mixArray.slice(lengthOfArray-2) );
-
 console.log( mixArray.slice(-4,-2 ) );
+
+
+
+
+
+let numbers = [10,40,50,60,80];
+
+let total = numbers.reduce(
+    (acc, current)=> acc + current
+
+    ,0);
+
+    console.log("total => ", total)
+    
+    
+    let cartItems = [
+        {name: 'laptop', price: 400},
+        {name: 'car', price: 1000},
+        {name: 'phone', price: 5000},
+    ]
+    
+    
+    let totalAmount = cartItems.reduce(
+        (sum, current) => sum + current.price
+        ,0
+    )
+    console.log("total amount => ", totalAmount);
+
+
+
+
+    let complexArray = [20,50,66, [70,80,88], [[40,44,[56,47]], 60,77]];
+//    console.log( complexArray.flat(2));
+   console.log(complexArray.flat(Infinity));
+
+
+
+   ///palindrome words
+/// mom
+/// madam
+/// pop
+
+    let checkPalindrome = (word) => word.split("").reverse().join("") == word;
+
+
+    console.log(checkPalindrome('mom'));
+    console.log(checkPalindrome('abc'));
 
 
 
