48fd9f37d9b320dcaf4859756ef4769c0621b501
Ashrib <asharib920@gmail.com>
2026-06-18 20:09:29 +0500

    complete authentication, and link data relation, create posts

--- Firebase/authentication with email-pass/dashboard.js
+++ Firebase/authentication with email-pass/dashboard.js
@@ -1,10 +1,14 @@
 import { requireGuest } from "./auth-guard.js";
-import { auth, collection, db, deleteDoc, deleteUser, doc, getAuth, getDocs, onAuthStateChanged, query, signOut, where } from "./firebaseConfig.js"
+import { addDoc, auth, collection, db, deleteDoc, deleteUser, doc, getAuth, getDocs, onAuthStateChanged, query, signOut, where } from "./firebaseConfig.js"
 
+let postsMain = document.querySelector(".posts-main");
 let signOutBtn = document.querySelector("#signOut-btn");
 let deleteAccBtn = document.querySelector("#deleteAcc-btn");
+let postBtn = document.querySelector("#post-btn");
+let postInp = document.querySelector("#post-inp");
 let currentUserData = null;
 let userId = null;
+let posts = [];
 
 // requireGuest(); /// auth-guard check
 
@@ -16,7 +20,7 @@ let getUserfromLocalStorage = () => {
 getUserfromLocalStorage();
 
 /// detele user data from db
-let deleteUserFromDb = async()=>{
+let deleteUserFromDb = async () => {
     try {
         await deleteDoc(doc(db, "users", currentUserData.id));
         console.log("successfully user deleted from db.")
@@ -35,7 +39,7 @@ let deleteUserAccount = async () => {
 
         await deleteUser(user).then(() => {  /// delete from authenication
             console.log("successfully account delete.");
-            deleteUserFromDb().then(()=>{
+            deleteUserFromDb().then(() => {
                 window.location.replace("./index.html")
             })
         })
@@ -53,7 +57,7 @@ let getUser = async () => {
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
             console.log(doc.id, " => ", doc.data());
-            currentUserData = {id: doc.id ,...doc.data()}
+            currentUserData = { id: doc.id, ...doc.data() }
         });
     } catch (error) {
         console.error(new Error('error in getting user data from db'));
@@ -61,12 +65,11 @@ let getUser = async () => {
 
     }
 }
-getUser().then(()=>{
+getUser().then(() => {
 
     console.log(currentUserData)
 })
 
-deleteAccBtn.addEventListener("click", () => deleteUserAccount())
 
 
 // sign out
@@ -80,4 +83,70 @@ let userSignOut = async () => {
     });
 }
 
+
+let createPost = async () => {
+    try {
+        if (postInp.value.length < 1) {
+            console.error(new Error("post text field must be filled!"));
+            return
+        }
+
+        let newDate = new Date();
+        let docRef = await addDoc(collection(db, "posts"), {  /// post
+            text: postInp.value,
+            uid: userId, /// foreign
+            post_id: newDate.getTime(), /// as primary key
+        })
+        console.log("post successfully add with doc_id => ", docRef.id);
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+
+let getUserPosts = async () => {
+    try {
+        const queryPost = query(
+            collection(db, 'posts'),
+            where("uid", "==", userId)
+        )
+
+        let querySnapshot = await getDocs(queryPost);
+        querySnapshot.forEach((post) => {
+            posts = [
+                ...posts,  ///
+                { id: post.id, ...post.data() }
+            ]
+        })
+
+        console.log(posts)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+let renderPosts = () => {
+    posts.map((post) => {
+        postsMain.innerHTML += `<div class='post'>
+    ${post?.text}
+    </div>`
+    })
+}
+
+getUserPosts().then(()=>renderPosts())
+
+deleteAccBtn.addEventListener("click", () => deleteUserAccount())
+
 signOutBtn.addEventListener("click", () => userSignOut())
+
+postBtn.addEventListener("click", () => createPost())
+
+
+
+
+
+
+
